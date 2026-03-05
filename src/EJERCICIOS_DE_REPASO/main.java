a78031ccda02ea81c5d0cd8eaca12f77b879ac94
JOSÉ JULIÁN SANTIAGO HERNÁNDEZ <jsantiagoh1@miumg.edu.gt>
2026-03-05 14:20:08 -0600

    MENSAJE

---
+++ src/EJERCICIOS_DE_REPASO/main.java
@@ -0,0 +1,10 @@
+package EJERCICIOS_DE_REPASO;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module EJERCICIOS_DE_REPASO {
+}
